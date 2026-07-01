18cc0e4f7de071ebbb4a6d930181ec2b4848c3e3
Hai Chu <hai.chu@tuni.fi>
2026-07-01 14:32:52 +0300

    requirements frozen from current pip

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
+# Install pytorch and other libs
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
+
+# Export conda environment
+conda env export -n dinov3 > env.yaml
