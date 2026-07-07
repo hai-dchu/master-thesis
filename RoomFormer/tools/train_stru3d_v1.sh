c2987205ea84eaf56012fce17e65a45fb4871995
Hai Chu <hai.chu@tuni.fi>
2026-07-07 12:40:10 +0300

    Remove remote logging for local development

--- RoomFormer/tools/train_stru3d_v1.sh
+++ RoomFormer/tools/train_stru3d_v1.sh
@@ -9,5 +9,5 @@ python main_v1.py --dataset_name=stru3d \
                --epochs=3 \
                --lr_dinov3_head=0.001 \
                --dinov3_repo='dinov3' \
-               --dinov3_checkpoint='checkpoints/dinov3_vits16_pretrain_lvd1689m-08c60483.pth' \
-               --wandb
+               --dinov3_checkpoint='checkpoints/dinov3_vits16_pretrain_lvd1689m-08c60483.pth'
+#               --wandb
