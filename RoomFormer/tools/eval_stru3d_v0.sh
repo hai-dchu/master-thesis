565307fc3f1939f0e45af482ab0e816e0c7869a3
Hai Chu <hai.chu@tuni.fi>
2026-07-07 15:39:42 +0300

    Add evaluation script for vanilla RoomFormer

--- RoomFormer/tools/eval_stru3d_v0.sh
+++ RoomFormer/tools/eval_stru3d_v0.sh
@@ -3,8 +3,8 @@
 python eval.py --dataset_name=stru3d \
                --dataset_root=data/stru3d \
                --eval_set=test \
-               --checkpoint=checkpoints/roomformer_stru3d.pth \
-               --output_dir=eval_stru3d \
+               --checkpoint=output-retrain-vanilla/2026-07-01-10-27-13_train_stru3d/checkpoint.pth \
+               --output_dir=eval_stru3d-retrain-vanilla \
                --num_queries=800 \
                --num_polys=20 \
                --semantic_classes=-1 
